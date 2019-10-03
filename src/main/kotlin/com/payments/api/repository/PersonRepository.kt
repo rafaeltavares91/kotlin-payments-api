effcf7ce052824e6996612baa1617c6fc8b74a03
Rafael Tavares <rafaeltavares91@gmail.com>
2019-10-03 20:31:59 -0300

    deleting person entity test and their layers

--- src/main/kotlin/com/payments/api/repository/PersonRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.payments.api.repository
-
-import com.payments.api.entity.Person
-import org.springframework.data.repository.CrudRepository
-
-interface PersonRepository : CrudRepository<Person, Long>
--- src/main/kotlin/com/payments/api/service/PersonService.kt
+++
@@ -1,7 +0,0 @@
-package com.payments.api.service
-
-import org.springframework.stereotype.Service
-
-@Service
-class PersonService {
-}
